58f48b08dc6bc354c119ec93c0d75827f7603ade
Patrick Lee <cluster000@gmail.com>
2020-03-05 22:06:27 +0800

    Add styles to DrawerToggle

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,39 +1,43 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, Component } from 'react';
 
 import Button from '../../UI/Button/Button';
 
-const OrderSummary = ({
-  ingredients,
-  price,
-  purchaseCanceled,
-  purchaseContinued
-}) => {
-  const ingredientSummary = Object.keys(ingredients).map(igKey => {
-    return (
-      <li key={igKey}>
-        <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
-        {ingredients[igKey]}
-      </li>
-    );
-  });
+class OrderSummary extends Component {
+  render() {
+    const {
+      ingredients,
+      price,
+      purchaseCanceled,
+      purchaseContinued
+    } = this.props;
 
-  return (
-    <Fragment>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingredients:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType='Danger' clicked={purchaseCanceled}>
-        Cancel
-      </Button>
-      <Button btnType='Success' clicked={purchaseContinued}>
-        Continue
-      </Button>
-    </Fragment>
-  );
-};
+    const ingredientSummary = Object.keys(ingredients).map(igKey => {
+      return (
+        <li key={igKey}>
+          <span style={{ textTransform: 'capitalize' }}>{igKey}</span>:{' '}
+          {ingredients[igKey]}
+        </li>
+      );
+    });
+
+    return (
+      <Fragment>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingredients:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType='Danger' clicked={purchaseCanceled}>
+          Cancel
+        </Button>
+        <Button btnType='Success' clicked={purchaseContinued}>
+          Continue
+        </Button>
+      </Fragment>
+    );
+  }
+}
 
 export default OrderSummary;
--- src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,5 +1,13 @@
 import React from 'react';
 
-const DrawerToggle = ({ clicked }) => <div onClick={clicked}>MENU</div>;
+import styles from './DrawerToggle.module.css';
+
+const DrawerToggle = ({ clicked }) => (
+  <div className={styles.DrawerToggle} onClick={clicked}>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
 
 export default DrawerToggle;
